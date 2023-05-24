fd17294f84459689d2beaa9892831f238fd81917
Scott Braaten <scottnicholasbraaten@gmail.com>
2023-05-23 20:46:04 -0500

    get fetch cycle to work

--- analyze.js
+++ analyze.js
@@ -9,31 +9,40 @@ const url = window.location.href;
 
 savedHTML = document.body.innerHTML;
 addedElement = document.createElement("h1");
+addedElement.setAttribute("id", "extensionOpenAI");
 addedElement.style.position = "fixed";
 addedElement.style.left = "500px";
 addedElement.style.top = "200px";
-addedElement.innerText = "HEY!";
+
 document.body.appendChild(addedElement);
 
-// async function process() {
-//   try {
-//     const response = await fetch("http://localhost:3000", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({ url: url }),
-//     });
+async function process() {
+  try {
+    console.log("a");
+    const response = await fetch("http://localhost:3000/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        url: url,
+      }),
+    });
 
-//     const data = await response.json();
-//     if (response.status !== 200) {
-//       throw (
-//         data.error || new Error(`Request failed with status ${response.status}`)
-//       );
-//     }
-//   } catch (error) {
-//     // Consider implementing your own error handling logic here
-//     console.error(error);
-//     alert(error.message);
-//   }
-// }
+    const data = await response.json();
+
+    if (response.status !== 200) {
+      throw (
+        data.error || new Error(`Request failed with status ${response.status}`)
+      );
+    }
+    console.log(data);
+    document.getElementById("extensionOpenAI").innerText = data.result;
+  } catch (error) {
+    // Consider implementing your own error handling logic here
+    console.error(error);
+    alert(error.message);
+  }
+}
+
+process();
